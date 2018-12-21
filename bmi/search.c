58555a897e637571f059c8c9e3a7235785b3a3aa
yuki3709 <32707335+yuki3709@users.noreply.github.com>
2018-12-21 14:34:13 +0900

    左括弧と右括弧を見つけた

--- bmi/search.c
+++ bmi/search.c
@@ -8,10 +8,11 @@ int main(void) {
     FILE *fp;
     char *filename = "bmi.c";
     char readline[N] = {'\0'};
-    char *condition;
+    char condition;
     char con[N];
-    char *left_parenthesis;
-    char *right_parenthesis;
+    int i;
+    int left_parenthesis=0;
+    int right_parenthesis=0;
     fp = fopen(filename, "r");
 
     if (fp == NULL) {
@@ -20,13 +21,25 @@ int main(void) {
     }
 
     while ( fgets(readline, N, fp) != NULL ) {
+        // if(strstr(readline, "if")){
+        //     left_parenthesis = strstr(readline, "(");
+        //     condition = left_parenthesis;
+        //     right_parenthesis = strstr(condition, ")");
+        //     con = condition;
+        //     printf("%s", condition);
+        //     printf("%s", right_parenthesis);
+        // }
         if(strstr(readline, "if")){
-            left_parenthesis = strstr(readline, "(");
-            condition = left_parenthesis;
-            right_parenthesis = strstr(condition, ")");
-            con = condition;
-            printf("%s", condition);
-            printf("%s", right_parenthesis);
+                for(i = 0; readline[i]; i++){
+                    if(readline[i]=='(')left_parenthesis=i;
+                    // printf("左%d",left_parenthesis);
+                    if(readline[i]==')')right_parenthesis=i;
+                    // printf("右%d",right_parenthesis);
+            }
+                // for(i=left_parenthesis+1; right_parenthesis-1; i++){
+                //     printf("%c",readline[i]);            
+                // }
+                
         }
     }
 
