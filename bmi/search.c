595c060cbe0e5a458b47510b3d4ec397ca0c38a9
yuki3709 <32707335+yuki3709@users.noreply.github.com>
2019-01-15 20:28:27 +0900

    すべてのテキストを文字列にして保持

--- bmi/search.c
+++ bmi/search.c
@@ -8,99 +8,27 @@ int main(void) {
     FILE *fp;
     char *filename = "bmi.c";
     char readline[N] = {'\0'};
-    char condition;
-    char variable1[20],variable2[20];
-    int i;
-    int main_flag = 0;
-    int function_flag = 0;
-    int var1_type_check, var2_type_check = 0;
-    int variable_count = 0;
-    int left_parenthesis;
-    int right_parenthesis;
-    int comma_num;
-    int address_num;
+    int i = 0, j = 0;
+    char temp, data[N][N];
     fp = fopen(filename, "r");
-
+    memset(data, 0, sizeof(data));
     if (fp == NULL) {
         fprintf(stderr, "%sのオープンに失敗しました.\n", filename);
         return -1;
     }
-    while ( fgets(readline, N, fp) != NULL ) {
-        //入力される変数を見つける
-         if(strstr(readline, "scanf")){
-                for(i = 0; readline[i]; i++){
-                    if(readline[i]==',')comma_num=i;
-                    if(readline[i]=='&')address_num=i;
-                    if(readline[i]==')')right_parenthesis=i;
-                }
-                if(variable_count==0){
-                    for(i=comma_num+3; i<right_parenthesis; i++){
-                        if(i!=address_num)variable1[i-(comma_num+3)]=readline[i];
-                    }
-                    printf("%s\n",variable1);
-                }
-                if(variable_count==1){
-                    for(i=comma_num+3; i<right_parenthesis; i++){
-                        if(i!=address_num)variable2[i-(comma_num+3)]=readline[i]; 
-                    }
-                }
-        variable_count++;
+    while((temp = fgetc(fp)) != EOF){
+        if (temp == '\n') {
+            data[i][j] = '\n';
+            i++;
+            j = 0;
+        } else {
+            data[i][j] = temp;
+            j++;
         }
     }
-    fclose(fp);
-    fp = fopen(filename, "r");
-    if (fp == NULL) {
-        fprintf(stderr, "%sのオープンに失敗しました.\n", filename);
-        return -1;
+    for (i = 0; i < N; i++) {
+        printf("%s", data[i]);
     }
-    while ( fgets(readline, N, fp) != NULL ) {
-        // 入力のデータ構造
-        // if(strstr(readline, "main"))main_flag = 1;
-        // if(main_flag==1){
-        //     if(strstr(readline, variable1))var1_type_check++;
-        //         if(var1_type_check==1){
-        //         printf("a");
-        //         var1_type_check++;
-        //     }
-        // }
-        if(strstr(readline, "scanf")){
-                for(i = 0; readline[i]; i++){
-                    if(readline[i]==',')comma_num=i;
-                    if(readline[i]=='&')address_num=i;
-                    if(readline[i]==')')right_parenthesis=i;
-                }
-                for(i=comma_num+1; i<right_parenthesis; i++){
-                    if(i==comma_num+1)printf("(");
-                    if(i!=address_num)printf("%c",readline[i]);
-                    if(i==right_parenthesis-1)printf(")\n");
-                }
-        }
-        //条件文の抜き出し
-        if(strstr(readline, "if")){
-                for(i = 0; readline[i]; i++){
-                    if(readline[i]=='(')left_parenthesis=i;
-                    if(readline[i]==')')right_parenthesis=i;
-                }
-                for(i=left_parenthesis+1; i<right_parenthesis; i++){
-                    printf("%c",readline[i]);
-                    if(i==right_parenthesis-1)printf("\n");
-                }
-                printf("!(");
-                for(i=left_parenthesis+1; i<right_parenthesis; i++){
-                    printf("%c",readline[i]);
-                    if(i==right_parenthesis-1)printf(")\n");
-                }
-        }
-        // 関数の抜き出し
-        if(!((strstr(readline, "if"))) &&
-            !((strstr(readline, "while"))) &&
-            !((strstr(readline, "for"))) &&
-            !((strstr(readline, "main"))) &&
-            ((strstr(readline,"){"))))function_flag=1;
-        if(function_flag==1)printf("%s",readline);
-        if((strstr(readline,"}")))function_flag=0;
-    }      
     fclose(fp);
-
     return 0;
 }
